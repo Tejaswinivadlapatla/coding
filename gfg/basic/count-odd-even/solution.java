f75c5c9b695ff3cb71cc660d3f9ebb8e41a3e38a
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-08-04 21:55:46 +0530

    ✅ Count Odd and Even | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-04

--- gfg/basic/count-odd-even/solution.java
+++ gfg/basic/count-odd-even/solution.java
@@ -1,16 +1,20 @@
 class Solution {
     public int[] countOddEven(int[] arr) {
         // Code here
-        int even = 0;
-        int odd = 0;
-        for(int num : arr){
+        int odd = 0 ;
+        int even = 0 ;
+        for (int num : arr  ){
             if(num %2 == 0){
                 even++;
             }
             else{
-                odd ++ ;
+                odd++;
             }
         }
-        return  new int[]{odd, even};
+        int ans [] = new int[2];
+        ans[0]=odd;
+        ans[1]=even;
+        return ans ;
+
     }
 }
