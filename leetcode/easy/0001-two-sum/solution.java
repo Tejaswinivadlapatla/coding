e053b6ff92521da2568d26205dbcbe924c82b527
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-08-20 00:30:59 +0530

    ✅ Two Sum | Easy | Java | Runtime: 50 ms (5.82%) | Memory: 47.2 MB | 2026-08-19

--- leetcode/easy/0001-two-sum/solution.java
+++ leetcode/easy/0001-two-sum/solution.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int ans[]= new int[2];
         for(int i = 0;i<=nums.length-1;i++){
             for(int j=i+1; j<=nums.length-1;j++){
                 if(nums[i]+nums[j] == target){
-                    return new int []{i,j};
+                   ans[0]=i;
+                   ans[1] = j;
                 }
             }
         }
-        return new int[]{};
+        return ans;
     }
 }
