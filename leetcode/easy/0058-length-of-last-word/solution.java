e2e9c78b33952d8f4c9f5ad90b4cf0dcb1f6833b
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-08-20 23:02:38 +0530

    ✅ Length of Last Word | Easy | Java | Runtime: 1 ms (28.21%) | Memory: 43.3 MB | 2026-08-20

---
+++ leetcode/easy/0058-length-of-last-word/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] words = s.split(" ");
+        //created the array by slpitting the s base on the " "
+        int n =words.length-1;
+        int max = 0 ;
+        // they are asking the last word so iterated to the last word and finally returned the sizr of last words
+        for(int i = 0 ; i<=words.length-1;i++){
+           max = words[n].length();
+        }
+        return max;
+    }
+}
