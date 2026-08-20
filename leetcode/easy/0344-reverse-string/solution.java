c9f5f4a12dab6e24ffa60213ee6499139d6e6e8b
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-08-20 22:47:08 +0530

    ✅ Reverse String | Easy | Java | Runtime: 4 ms (5.32%) | Memory: 48 MB | 2026-08-20

--- leetcode/easy/0344-reverse-string/solution.java
+++ leetcode/easy/0344-reverse-string/solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public void reverseString(char[] s) {
-        char[] arr = new char[s.length];
-        int n =s.length-1;
-        for (int i=0;i<s.length;i++){
-            arr[i]=s[n-i];
-            
-        }
-        //why again i wrote the for loop : leetcode does not check the arr it checks only the s so we changed it again 
-        for (int j=0;j<s.length;j++){
-            s[j]=arr[j];            
+        // by using two pointers approach
+        int left = 0 ;
+        int right = s.length-1;
+        for(int i = 0;i<s.length-1;i++){
+            while(left<right){// if it meets the midpoint it stops the poniter moving
+            char temp = s[right];
+            s[right] = s[left];
+            s[left] = temp;
+            left++;
+            right--;
+            }
         }
         System.out.println(s);
     }
