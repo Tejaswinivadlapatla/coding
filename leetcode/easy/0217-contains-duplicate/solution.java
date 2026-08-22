d77c39af309f4c4192e8fa65575fe15bd86fcf2c
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-08-22 10:28:24 +0530

    ✅ Contains Duplicate | Easy | Java | Runtime: 21 ms (34.98%) | Memory: 108.4 MB | 2026-08-22

--- leetcode/easy/0217-contains-duplicate/solution.java
+++ leetcode/easy/0217-contains-duplicate/solution.java
@@ -1,20 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i =0;i<nums.length;i++){//iterating every element in the loop
-            if(map.containsKey(nums[i])){//checking weather the hashmap was having the element of not if element was already exits it executes the if or it executes the else statement
-                map.put(nums[i],map.get(nums[i])+1);
-                //keeping element of array in hashmap ,  element was already present so map.get we will get the value of element then we will add1 to that value if 1 -> 1 then by ading +1 we will get 1 -> 2
+        HashSet<Integer> set = new HashSet<>();
+        for(int i =0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;//will return true if we are having the element already in the set 
             }
-            else{
-                map.put(nums[i],1);//if element was not present we will directly add element to it
-            }
-        }
-        for(int values :map.values()){//iterating for loop for all values in hashmap we are not using index beacuse A HashMap doesn't have indexes like an array
-            if(values>1){
-                return true;
-            }
+            set.add(nums[i]);//adding nums to hashset
         }
         return false;
+        
     }
 }
