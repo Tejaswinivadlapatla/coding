ed1dc7f2b189a12001b05c567c1e877cd0101fa9
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-09-17 10:11:00 +0530

    ✅ Sort the People | Easy | Java | Runtime: 10 ms (56.88%) | Memory: 47.5 MB | 2026-09-17

---
+++ leetcode/easy/2418-sort-the-people/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer,String> map = new HashMap<>();
+        for(int i=0 ; i<= names.length-1;i++){
+            map.put(heights[i] , names[i]);
+        }
+           Arrays.sort(heights);
+        String[] ans = new String[names.length];
+
+        int k = 0;
+        
+        for(int i = heights.length - 1; i >= 0; i--) {
+            ans[k] = map.get(heights[i]);
+            k++;
+        }
+        return ans ; 
+       }
+}
