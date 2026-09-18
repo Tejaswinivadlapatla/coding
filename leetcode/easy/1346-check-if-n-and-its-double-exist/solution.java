061db8c6b2fd13374d29814efd8ed075e6a0d387
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-09-18 17:50:35 +0530

    ✅ Check If N and Its Double Exist | Easy | Java | Runtime: 2 ms (96.69%) | Memory: 44.8 MB | 2026-09-18

--- leetcode/easy/1346-check-if-n-and-its-double-exist/solution.java
+++ leetcode/easy/1346-check-if-n-and-its-double-exist/solution.java
@@ -2,10 +2,13 @@ class Solution {
     public boolean checkIfExist(int[] arr) {
        HashSet<Integer> set = new HashSet<>();
        for(int i=0 ; i<=arr.length-1;i++){
-        set.add(arr[i]);
         if(set.contains(arr[i]*2)){
         return true;
        }
+       if(arr[i] % 2 == 0 && set.contains(arr[i] / 2)) {
+                return true;
+            }
+ set.add(arr[i]);
     
        }
        
