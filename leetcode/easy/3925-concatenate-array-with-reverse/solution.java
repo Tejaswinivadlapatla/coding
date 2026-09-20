1fb3f7f9ba4e6c8d5ded181e3713b3b2478a2912
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-09-20 14:32:41 +0530

    ✅ Concatenate Array With Reverse | Easy | Java | Runtime: 1 ms (98.13%) | Memory: 47.4 MB | 2026-09-20

---
+++ leetcode/easy/3925-concatenate-array-with-reverse/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] concatWithReverse(int[] nums) {
+        int result[] = new int[2*(nums.length)];
+        int n = nums.length;
+        for(int i=0 ;i<=nums.length-1 ; i++){
+            result[i] = nums[i];
+            result[n+i] = nums[n-1-i];
+        }
+        return result;
+    }
+}
