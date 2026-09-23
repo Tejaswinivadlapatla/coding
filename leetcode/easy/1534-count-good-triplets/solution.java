582721df91d1ca1c7ec027b3efc45b407dbd1faa
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-09-23 12:02:02 +0530

    ✅ Count Good Triplets | Easy | Java | Runtime: 10 ms (95.39%) | Memory: 43 MB | 2026-09-23

--- leetcode/easy/1534-count-good-triplets/solution.java
+++ leetcode/easy/1534-count-good-triplets/solution.java
@@ -3,12 +3,13 @@ class Solution {
         int count =0 ;
         for(int i=0 ; i<=arr.length-1 ; i++){
             for(int j=i+1 ; j <= arr.length-1 ; j++){
+                if(Math.abs(arr[i]-arr[j])<=a){// we will check first condition if it satisfies it will go to the next condition unless it will neglect that triplet , complexity will decrese 
                 for(int k=j+1 ; k<=arr.length-1;k++){
-                    // for loops will iterate all values in the array like the triplets in that by using the condition we will find the good triplets
-                    // abs is used for the positive numbers 
-                    if( Math.abs(arr[i] - arr[j] )<=a  && Math.abs(arr[j] - arr[k])<=b && Math.abs(arr[i] - arr[k]) <=c ){
+                    
+                    if(  Math.abs(arr[j] - arr[k])<=b && Math.abs(arr[i] - arr[k]) <=c ){
                     count ++; // for every good triplet count will increase
                     }
+                }
                 }
             }
         }
